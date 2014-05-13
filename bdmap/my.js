eb610670d8740a99e6fdb7957bca681fde9ac7c5
cc520 <463716140@qq.com>
2014-05-13 14:34:12 +0800

    Geocoder getPoint 根据地点查询坐标

--- bdmap/my.js
+++ bdmap/my.js
@@ -12,26 +12,52 @@ function init(){
         imageOffset:new BMap.Size(0,0)
     });
     */
+    var myGeo=new BMap.Geocoder();
+    myGeo.getPoint("江苏常州金梅花园",function(point) { 
+        if(point){
+            map.centerAndZoom(point,16);
+            map.addOverlay(new BMap.Marker(point));
+        }
+    },'常州市');
+    var title='城市地图信息';
+    $.getJSON('city.json',function(dcity){
 
-    function addMarket(point,i){
-        var marker= new BMap.Marker(point);
-        marker.addEventListener('click',function() {
-            alert(i);
-        });
-        map.addOverlay(marker);
-    }
-    var bounds=map.getBounds();
-    var southwest=bounds.getSouthWest();
-    var northeast=bounds.getNorthEast();
-    var spn=bounds.toSpan();
-    /*console.log(southwest);
-    console.log(northeast);*/
-    
-    var lngSpan=spn.lng;
-    var latSpan=spn.lat;
-    for(var i=0;i<10;i++){
-        var point=new BMap.Point(southwest.lng + lngSpan*(Math.random()),southwest.lat+latSpan*(Math.random()));
-        addMarket(point,i);
-    }
+        randomTenCity();
+
+        function addMarket(point,i){
+            var marker= new BMap.Marker(point);
+            marker.addEventListener('click',function() {
+
+                var opts={
+                    width:250,
+                    height:100,
+                    title:title
+                };
+                var infoWindow=new BMap.InfoWindow();
+                infoWindow.setContent('<div id="city"><p><label for="">城市：</label><span>' + dcity[i]['name'] + '</span></p><p><label for="">地址：</label><span>' + dcity[i]['addr']+ '</span></p></div>');
+                infoWindow.setWidth(250);
+                infoWindow.setHeight(100);
+                map.openInfoWindow(infoWindow,point);
+            });
+
+            map.addOverlay(marker);
+
+        }
+        function randomTenCity(){
+            var bounds=map.getBounds();
+            var southwest=bounds.getSouthWest();
+            var northeast=bounds.getNorthEast();
+            var spn=bounds.toSpan();
+            /*console.log(southwest);
+            console.log(northeast);*/
+            
+            var lngSpan=spn.lng;
+            var latSpan=spn.lat;
+            for(var i=0;i<10;i++){
+                var point=new BMap.Point(southwest.lng + lngSpan*(Math.random()),southwest.lat+latSpan*(Math.random()));
+                addMarket(point,i);
+            }
+        }
+    });
 }
 
