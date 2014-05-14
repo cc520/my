3ad7dd2e20de82c6bf49b4ed4ab1b12729af2b22
cc520 <463716140@qq.com>
2014-05-14 11:08:15 +0800

    修改了商城列表页逻辑不会跳到搜索页面

--- api/extend.func.php
+++ api/extend.func.php
@@ -1,4 +1,42 @@
 <?php
 defined('IN_DESTOON') or exit('Access Denied');
 #Your Functions
+#
+function property_option_value($moduleid,$options){
+    global $db;
+    foreach($options as $k => $v){
+        $sql = "SELECT itemid FROM {$db->pre}category_value WHERE moduleid={$moduleid} AND oid=$k AND value={$v}";
+        $result = $db->query($sql);
+        $lists[$k] = array();
+        while($r = $db->fetch_array($result)) {
+            $lists[$k][] = $r['itemid'];
+        }
+    }
+
+    return array_reduce($lists,'array_intersect',array_pop($lists));
+}
+
+function get_dco_value($name){
+    global $_GET;
+    $darr = array();
+    $len = strlen($name);
+    foreach($_GET as $k=>$v){
+        if(strlen($v)){
+            if(strpos($k,$name) !== FALSE){
+                $doption =substr($k,$len + 1);
+                $darr[$doption] = $v;
+            }
+        }
+    }
+    return $darr;
+}
+function get_all_param(){
+    global $_GET;
+    $str = '';
+    foreach($_GET as $k => $v){
+        if($v)
+            $str.='&'.$k.'='.$v;
+    }
+    return ltrim($str,'&');
+}
 ?>
--- module/mall/list.inc.php
+++ module/mall/list.inc.php
@@ -2,6 +2,9 @@
 defined('IN_DESTOON') or exit('Access Denied');
 if(!$CAT || $CAT['moduleid'] != $moduleid) include load('404.inc');
 require DT_ROOT.'/module/'.$module.'/common.inc.php';
+require DT_ROOT.'/include/post.func.php';
+include load('search.lang');
+
 if($MOD['list_html']) {
 	$html_file = listurl($CAT, $page);
 	if(is_file(DT_ROOT.'/'.$MOD['moduledir'].'/'.$html_file)) d301($MOD['linkurl'].$html_file);
@@ -12,11 +15,33 @@ if($MOD['cat_property'] && $CAT['property']) {
 	require DT_ROOT.'/include/property.func.php';
 	$PPT = property_condition($catid);
 }
+$cityid = @$_GET['cityid'];
 unset($CAT['moduleid']);
 extract($CAT);
 $maincat = get_maincat($child ? $catid : $parentid, $moduleid);
+/*print_r($maincat);*/
 $condition = 'status=3';
 $condition .= ($CAT['child']) ? " AND catid IN (".$CAT['arrchildid'].")" : " AND catid=$catid";
+if(isset($day)){
+    $starttime = $DT_TIME - $day * 60 * 60 * 24;
+    $condition .= " AND addtime >= '$starttime'";
+}
+
+//检测属性分类
+
+$doption = get_dco_value('ppt');
+if(!empty($doption)){
+    $pptitemids = property_option_value($moduleid,$doption);
+    if(!empty($pptitemids)){
+        $condition .= ' AND itemid IN ('.implode(',',$pptitemids).') ';
+    }else{
+        $condition .= ' AND 1=0';
+    }
+}
+$vip = isset($vip) ? intval($vip) : 0;
+$condition.= " AND vip={$vip}";
+
+
 if($cityid) {
 	$areaid = $cityid;
 	$ARE = $AREA[$cityid];
@@ -36,9 +61,24 @@ if($cityid) {
 $pagesize = $MOD['pagesize'];
 $offset = ($page-1)*$pagesize;
 $pages = listpages($CAT, $items, $page, $pagesize);
+
+$dorder  = array($MOD['order'], '', 'price DESC', 'price ASC', 'vip DESC', 'vip ASC', 'amount DESC', 'amount ASC', 'minamount DESC', 'minamount ASC');
+isset($order) && isset($dorder[$order]) or $order = 0;
+
+$timeorder = array('0'=>'更新时间','1'=>'1天内','7'=>'7天内','15'=>'15天内','30'=>'30天内');
+$sorder  = array($L['order'], $L['order_auto'], $L['price_dsc'], $L['price_asc'], $L['vip_dsc'], $L['vip_asc'], $L['amount_dsc'], $L['amount_asc'], $L['minamount_dsc'], $L['minamount_asc']);
+$vipcheck = array(1=>'vip');
+$order_select  = dselect($sorder, 'order', '', $order,"onchange=\"Go(sh+'&order='+this.value)\"");
+$timeorder_select = dselect($timeorder,'day','',@$day,"onchange=\"Go(sh+'&day='+this.value)\"");
+
+$vipcheckbox = dcheckbox($vipcheck,VIP,$vip,"onclick=\"var vip=1-{$vip};Go(sh+'&vip='+vip);\"");
+
+
 $tags = array();
 if($items) {
-	$result = $db->query("SELECT ".$MOD['fields']." FROM {$table} WHERE {$condition} ORDER BY ".$MOD['order']." LIMIT {$offset},{$pagesize}", ($CFG['db_expires'] && $page == 1) ? 'CACHE' : '', $CFG['db_expires']);
+	$worder = $dorder[$order] ? " ORDER BY $dorder[$order]" : '';
+    $sql = "SELECT ".$MOD['fields']." FROM {$table} WHERE {$condition}{$worder} LIMIT {$offset},{$pagesize}";
+	$result = $db->query($sql,($CFG['db_expires'] && $page == 1) ? 'CACHE' : '', $CFG['db_expires']);
 	while($r = $db->fetch_array($result)) {
 		$r['adddate'] = timetodate($r['addtime'], 5);
 		$r['editdate'] = timetodate($r['edittime'], 5);
@@ -53,6 +93,7 @@ if($items) {
 $showpage = 1;
 $datetype = 5;
 $seo_file = 'list';
+
 include DT_ROOT.'/include/seo.inc.php';
 if($EXT['wap_enable']) $head_mobile = $EXT['wap_url'].'index.php?moduleid='.$moduleid.'&catid='.$catid.($page > 1 ? '&page='.$page : '');
 $template = $CAT['template'] ? $CAT['template'] : 'list';
